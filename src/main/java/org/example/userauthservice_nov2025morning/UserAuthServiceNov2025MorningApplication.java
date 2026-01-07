be6739068d9b3c2581ee8c444909230321b85a91
khannaanurag1 <khannaanurag1@gmail.com>
2026-01-07 07:08:19 +0530

    initial commit

---
+++ src/main/java/org/example/userauthservice_nov2025morning/UserAuthServiceNov2025MorningApplication.java
@@ -0,0 +1,13 @@
+package org.example.userauthservice_nov2025morning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserAuthServiceNov2025MorningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserAuthServiceNov2025MorningApplication.class, args);
+    }
+
+}
